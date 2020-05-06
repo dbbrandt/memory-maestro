86583fb8f01ae9d8e36fc58be996689df2b34a08
Daniel Brandt <dbbrandt@gmail.com>
2020-05-06 18:01:55 -0400

    fix: Expose aws-exports.js on the front end for deploy.

--- src/components/login/Authenticate.js
+++ src/components/login/Authenticate.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
 import Amplify, { Analytics } from "aws-amplify";
-import awsconfig from "../../aws-exports";
+import awsconfig from "./aws-exports";
 import { withAuthenticator } from "aws-amplify-react";
 import { handleAuthenticateUser} from "../../actions/authedUser";
 import { handleInititalData } from "../../actions/shared";
