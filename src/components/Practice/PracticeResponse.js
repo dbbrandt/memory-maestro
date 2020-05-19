671527809b17263b18fff6c789abc7b43e30437c
Dan Brandt <dbbrandt@users.noreply.github.com>
2020-05-19 13:14:51 -0400

    MMR-14 handle multiple criterion (#22)
    feature: Handle multiple descriptor for correct answer.
    fix: Autosize font for large copy in prompt and descriptor.
     fix: Hide review buttons when in text input mode prior to answer.

--- src/components/Practice/PracticeResponse.js
+++ src/components/Practice/PracticeResponse.js
@@ -12,10 +12,19 @@ class PracticeResponse extends Component {
     answer: ""
   };
 
+  calcTextSize = text => {
+    return 18 - Math.floor(text.length / 100);
+  };
+
   getStimulus = (title, prompt) => {
     if (!prompt) return null;
     if (prompt.stimulus_url === "") {
-      return <div className="detail-text detail-copy">{prompt.copy}</div>;
+      const fontSize = { fontSize: this.calcTextSize(prompt.copy) };
+      return (
+        <div className="detail-text detail-copy" style={fontSize}>
+          {prompt.copy}
+        </div>
+      );
     } else {
       return (
         <img alt={title} src={prompt.stimulus_url} className="stimulus-img" />
@@ -28,7 +37,8 @@ class PracticeResponse extends Component {
     const { goalId, interaction, textInput, dispatch } = this.props;
     const interactionId = interaction.id;
     this.setState({ face: face === FRONT ? BACK : FRONT });
-    if (textInput) dispatch(handleSubmitRoundCheck(goalId, interactionId, answer ));
+    if (textInput)
+      dispatch(handleSubmitRoundCheck(goalId, interactionId, answer));
   };
 
   handleChange = event => {
@@ -36,13 +46,15 @@ class PracticeResponse extends Component {
   };
 
   handleSubmit = (interaction, review) => {
-    const { answer } = this.state
+    const { answer } = this.state;
     this.setState({ face: FRONT, answer: "" });
     this.props.onSubmit(interaction, answer, review);
   };
 
   getDescriptor = interaction => {
-    return interaction.criterion[0].descriptor;
+    const descriptor = interaction.criterion[0].descriptor;
+    const fontSize = { fontSize: this.calcTextSize(descriptor) };
+    return <span style={fontSize}>{descriptor}</span>;
   };
 
   showContent = interaction => {
@@ -69,9 +81,10 @@ class PracticeResponse extends Component {
     const { interaction, current, totalCards, textInput } = this.props;
     const { face, answer } = this.state;
     const toFace = face === FRONT ? BACK : FRONT;
+    const showButtons = !textInput || face === BACK ? true : false;
     return (
       <div className="detail-container">
-        <div style={{ textAlign: "left" }}>
+        <div>
           {current + 1} of {totalCards}
         </div>
         <div className="detail">
@@ -97,37 +110,38 @@ class PracticeResponse extends Component {
               {toFace}
             </button>
           </div>
-          <div className="detail-bottom">
-            <div className="detail-buttons">
-              <div>
-                <button
-                  className="response-button correct-button"
-                  onClick={() =>
-                    this.handleSubmit(interaction, true)
-                  }
-                >
-                  Correct!
-                </button>
-              </div>
-              <div>
-                <button
-                  className="response-button incorrect-button"
-                  onClick={() =>
-                    this.handleSubmit(interaction, false)
-                  }
-                >
-                  Incorrect
-                </button>
+          {showButtons && (
+            <div className="detail-bottom">
+              <div className="detail-buttons">
+                <div>
+                  <button
+                    className="response-button correct-button"
+                    onClick={() => this.handleSubmit(interaction, true)}
+                  >
+                    Correct!
+                  </button>
+                </div>
+                <div>
+                  <button
+                    className="response-button incorrect-button"
+                    onClick={() => this.handleSubmit(interaction, false)}
+                  >
+                    Incorrect
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = ({ selections, round }, {interaction, current, totalCards}) => {
+const mapStateToProps = (
+  { selections, round },
+  { interaction, current, totalCards }
+) => {
   const { goal, textInput } = selections;
   return {
     goalId: goal,
